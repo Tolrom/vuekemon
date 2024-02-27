3cc25b752d67556c9554c16ad3665fd115a361d5
Tolrom <max.le.morlot@gmail.com>
2024-02-27 15:32:04 +0100

    ajout momo + déplacement persos + logs

--- app.js
+++ app.js
@@ -28,35 +28,44 @@ Vue.createApp({
             this.round = 0;
             this.end = false;
             this.outcome = '';
+            this.battleLogs = [];
         },
         attackAdversaire(){
+            this.battleLogs.push("Jeff Pizza se jette sur Sayann ! Il lui inflige "+this.jeff.att+" points de dégats")
             let crit = Math.round(Math.random()*100)
             if(crit >= 95){
+                this.battleLogs.push("Coup critique! Impossible pour Sayann de se défendre!");
                 this.sayann.health -= this.jeff.att;
             }
             else {
+                this.battleLogs.push("Sayann se défend! Il réduit les dégats de "+this.sayann.def);
                 this.sayann.health -= (this.jeff.att - this.sayann.def);
                 this.round ++;
             }
         },
         kamehameha(){
+            this.battleLogs.push("Sayann envoie un Kamehameha sur son adversaire! Il lui inflige "+this.sayann.att+" dégats !");
             let crit = Math.round(Math.random()*100);
             if(crit >= 95){
+                this.battleLogs.push("Coup critique! Impossible pour Jeff Pizza de se défendre!");
                 this.jeff.health -= this.sayann.att;
-                this.attackAdversaire();
             }
             else{
+                this.battleLogs.push("Jeff Pizza se défend! Il réduit les dégats de "+this.jeff.def);
                 this.jeff.health -= (this.sayann.att - this.jeff.def);
-                this.attackAdversaire();
             }
+            this.attackAdversaire();
         },
         genkidama(){
+            this.battleLogs.push("Sayann envoie un Genkidama sur son adversaire! Il lui inflige "+this.sayann.att * 3+" dégats !");
             let crit = Math.round(Math.random()*100);
             if(crit >= 95){
+                this.battleLogs.push("Coup critique! Impossible pour Jeff Pizza de se défendre!");
                 this.jeff.health -= (this.sayann.att*3);
-                this.attackAdversaire();
+            }else {
+                this.battleLogs.push("Jeff Pizza se défend! Il réduit les dégats de "+this.jeff.def);
+                this.jeff.health -= ((this.sayann.att * 3) - this.jeff.def);
             }
-            this.jeff.health -= ((this.sayann.att * 3) - this.jeff.def);
             this.attackAdversaire();
         },
         senzu(){
@@ -66,10 +75,12 @@ Vue.createApp({
                     this.sayann.health += heal;
                 }
             }
+            this.battleLogs.push("Sayann mange un senzu! Il récupère "+heal+" hp !");
             this.attackAdversaire();
         },
         giveUp(){
             this.sayann.health = 0;
+            this.battleLogs.push("Sayann a abandonné pour laisser son fils prendre le relais!");
         },
     },
     computed: {
@@ -82,6 +93,7 @@ Vue.createApp({
                 this.outcome = "VICTOIRE";
                 this.colorWin = "green";
                 this.end = true;
+                this.battleLogs.push("Sayann a triomphé de son adversaire !");
             }
         },
         'sayann.health': function(hp) {
@@ -90,6 +102,7 @@ Vue.createApp({
                 this.outcome = "DÉFAITE";
                 this.colorWin = "red";
                 this.end = true;
+                this.battleLogs.push("Sayann a échoué, la terre court un grand danger !");
             }
         },
         round(round){
